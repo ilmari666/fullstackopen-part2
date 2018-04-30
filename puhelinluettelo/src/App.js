ce078d43516fecb41c643c33acd83998f74102f4
Ilmari <ilmari@sokeri.com>
2018-04-30 23:47:52 +0300

    add number

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Button from './components/Button';
+import Contact from './components/Contact';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       persons: [
-        { name: 'Arto Hellas' },
+        { name: 'Arto Hellas', number: '040-123456'},
       ],
       newName: '',
+      newNumber: '',
     };
   }
 
   onInputUpdate() {
     return (e) => {
-      this.setState({ newName: e.target.value });
+      const { value, name } = e.target;
+      this.setState({ [name]: value });
     };
   }
 
@@ -22,37 +26,41 @@ class App extends React.Component {
     return (e) => {
       e.preventDefault();
       this.setState((prevState) => {
-        const { newName, persons } = prevState;
+        const { newName, persons, newNumber } = prevState;
         const duplicate = persons.some(({ name }) => name === newName);
         if (duplicate) {
           return prevState;
         }
         return {
           newName: '',
-          persons: persons.concat([{ name: newName }]),
+          persons: persons.concat({ name: newName, number: newNumber }),
         };
       });
     };
   }
 
   render() {
-    const { persons, newName } = this.state;
+    const { persons, newName, newNumber } = this.state;
     return (
       <div>
         <h2>Puhelinluettelo</h2>
         <form onSubmit={this.submitForm()}>
           <div>
-            nimi: <input name="name" onChange={this.onInputUpdate()} value={newName} />
+            nimi: <input name="newName" onChange={this.onInputUpdate()} value={newName} />
+          </div>
+          <div>
+            numero: <input name="newNumber" onChange={this.onInputUpdate()} value={newNumber} />
           </div>
           <div>
             <Button type="submit">lisää</Button>
           </div>
         </form>
         <h2>Numerot</h2>
-        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+        {persons.map((contact) => <Contact key={contact.name} {...contact} />)}
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ puhelinluettelo/src/components/Contact.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Contact = ({name, number}) => (<p>{name} {number}</p>);
+
+export default Contact;
