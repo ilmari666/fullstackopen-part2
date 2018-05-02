3a77e86da05348d1140af72f791d3901ea266bd7
ilmari666 <ilmari@vikingtheories.com>
2018-05-02 17:34:49 +0300

    ..2.19

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -4,6 +4,7 @@ import personsService from './services/persons'
 import Button from './components/Button';
 import Contacts from './components/Contacts';
 import Input from './components/Input';
+import Notification from './components/Notification';
 
 class App extends React.Component {
   constructor(props) {
@@ -37,29 +38,36 @@ class App extends React.Component {
       const id = existingContact.id;
       if (window.confirm(`${name} on jo luettelossa. Korvataanko vanha numero uudella?`)){
         personsService.updatePerson(contact, id)
-          .then(updatedContact =>
+          .then(updatedContact =>{
             this.setState( {
               newName: '',
               newNumber: '',
               persons: persons.map(person=>person.id === id ? updatedContact : person),
-            })
-          )
+
+            });
+            this.notify(`${contact.name} numero on päivitetty.`);
+          }).catch((e)=> {
+            this.notify(`Kontaktia ${contact.name} ei löydetty, päivittäminen epäonnistui. Voit lisätä käyttäjän uudelleen.`, true);
+            this.setState({persons: persons.filter(person=>person.id !== id)});
+          });
       }
     } else {
       // create new contact
       personsService.createPerson(contact)
-        .then(person =>
+        .then(person => {
           this.setState( {
             newName: '',
             newNumber: '',
             persons: persons.concat(person),
           })
-        )
+          this.notify(`${person.name} on lisätty luetteloon.`);
+        })
       }
   };
   confirmRemoval = (id, name) => () => {
     if (window.confirm(`poistetaanko ${name}?`)){
       this.deletePerson(id);
+      this.notify(`${name} on poistettu luettelosta.`);
     }
   }
 
@@ -70,11 +78,18 @@ class App extends React.Component {
     })
   };
 
+  notify(note, error){
+    clearTimeout(this.noteTimeout);
+    this.setState({notification: note, error});
+    this.noteTimeout = setTimeout(()=>this.setState({notification: ''}), 3000);
+  }
+
   render() {
-    const { persons, newName, newNumber, filter } = this.state;
+    const { persons, newName, newNumber, filter, notification, error } = this.state;
 
     return (
       <div>
+        <Notification note={notification} error={error}/>
         <h2>Puhelinluettelo</h2>
         <form onSubmit={this.submitForm}>
           <Input label="hae:" name="filter" onChange={this.onInputUpdate} value={filter} />
